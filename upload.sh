6e78c11d1db72bfc942251380dc09da460a6ff34
Ben Doan <ben@bendoan.me>
2015-05-10 00:28:49 -0500

    fixed pre overflow bug

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 echo "Building site..." &&
     python build.py build &&
     echo "Uploading site..." &&
-    rsync -r --progress deploy/ bendoan:/home/public
+    rsync -r --progress deploy/ bendoan.me:/home/public
